899c33b854d553fda49ddb93eb4f25520d7b95e2
rksipes <rksipes@eagles.ewu.edu>
2015-02-21 19:35:42 -0800

    Added inOrder( ) method.

--- Results.java
+++ Results.java
@@ -3,7 +3,7 @@ import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Scanner;
 
-/*Takes two text file arguments and compares their contents*/
+/*Methods for comparing query results on matching content and/or order of content*/
 
 public class Results {
 	private static HashMap<String, ?> map;
@@ -47,9 +47,36 @@ public class Results {
 			return false;
 	}
 	
+	public static boolean inOrder(String file1, String file2) {
+		if (file1.equals(file2)) return true;
+		
+		Scanner sc1 = getScanner(file1);
+		Scanner sc2 = getScanner(file2);
+
+		while (sc1.hasNext()) {
+			if (sc2.hasNext()) {
+				String line1 = sc1.nextLine();
+				String line2 = sc2.nextLine();
+				if (!line1.equals(line2)) {
+					sc1.close(); sc2.close();
+					return false; 				//records out of order
+				}
+			} else { 
+				sc1.close(); sc2.close();
+				return false; 					//file1 longer than file2
+			}
+		}
+		if (sc2.hasNext()) { 
+			sc1.close(); sc2.close(); 				//file2 longer than file1
+			return false;
+		}
+		sc1.close(); sc2.close();
+		return true;							//files are in matching order
+	}
+	
 	/*testing*/
 	public static void main(String[] args) {
-		match(args[0], args[1]);
+		//match(args[0], args[1]);
 	}
 	
 
