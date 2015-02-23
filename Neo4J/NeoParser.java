b96b9ba883159ecafe831cccb9803776baf4f2e8
izzyPflee <ipfleegor@ewu.edu>
2015-02-23 14:17:52 -0800

    update neo4j files

--- Neo4J/NeoParser.java
+++ Neo4J/NeoParser.java
@@ -28,7 +28,9 @@ public class NeoParser
 	
     private enum RelTypes implements RelationshipType
     {
-        ARRESTED, COMMITTED, HAPPENED_IN, WHEN, WHERE, 
+        ARRESTED, COMMITTED, YEAR_WHEN, MONTH_WHEN, 
+        LOCATION_WHERE, COMMUNITY_WHERE, BEAT_WHERE, 
+        DISTRICT_WHERE, HOME_WHERE, TIME_WHEN
     }
     
 	public NeoParser(String fileName, GraphDatabaseService DB)
@@ -36,76 +38,64 @@ public class NeoParser
 		buildDB(fileName, DB);
 	}
 	
-
 	/**
 	 * @param filename
 	 * @return int
 	 * 
-	 * Parses the rows of the crimes file and recombines the dates columns into one date object.
+	 * Parses the rows of the crimes file
 	 * Each row has 13 columns after the parsing:
 	 * id(0), date(1), time(2), AM/PM(3), primary_type(4), 
 	 * description(5), location_description(6), arrest(7), 
 	 * domestic(8), beat(9), district(10), ward(11), 
-	 * community_area(12), latitude(13), longitude(14)
+	 * community(12), latitude(13), longitude(14)
 	 */
 	private void buildDB(String fileName, GraphDatabaseService DB)
 	{
 		final int batchSize = 50000;//flush the heap every 50,000 records entered
-		Relationship relationship;
+		int heapCount =0;//keep track of unflushed nodes on the heap
+
 		try{
 			Scanner sc = new Scanner(new File(fileName));
 		
 			//The indexes for removing redundant nodes
-			UniqueFactory.UniqueNodeFactory months = getIndex(DB, "Month");
-			UniqueFactory.UniqueNodeFactory years = getIndex(DB, "Year");
-			UniqueFactory.UniqueNodeFactory crimes = getIndex(DB, "Crime");
-			UniqueFactory.UniqueNodeFactory districts = getIndex(DB, "District");
-			UniqueFactory.UniqueNodeFactory beats = getIndex(DB, "Beat");
-			UniqueFactory.UniqueNodeFactory communities = getIndex(DB, "Community");
-			UniqueFactory.UniqueNodeFactory loc_descriptions = getIndex(DB, "LocationDescription");
+			UniqueFactory.UniqueNodeFactory ampm= getIndex(DB, "AM/PM");
+			UniqueFactory.UniqueNodeFactory months = getIndex(DB, "MONTH");
+			UniqueFactory.UniqueNodeFactory years = getIndex(DB, "YEAR");
+			UniqueFactory.UniqueNodeFactory beats = getIndex(DB, "BEAT");
+			UniqueFactory.UniqueNodeFactory communities = getIndex(DB, "COMMUNITY");
+			UniqueFactory.UniqueNodeFactory loc_descriptions = getIndex(DB, "LOCATION_DESCRIPTION");
+			UniqueFactory.UniqueNodeFactory crimes = getIndex(DB, "CRIME");
+			UniqueFactory.UniqueNodeFactory districts = getIndex(DB, "DISTRICT");
+			UniqueFactory.UniqueNodeFactory domestic = getIndex(DB, "DOMESTIC");
+			UniqueFactory.UniqueNodeFactory arrest = getIndex(DB, "ARREST");
 			
-	
-		
 			//remove the column headers 'keys' from the file first
 			String firstLine = sc.nextLine();
 			String[] keys = COMMA_PATTERN.split(firstLine);
 		
-			int heapCount =0;
-			
 			Transaction tx = DB.beginTx();//initial transaction
 		
 			while (sc.hasNextLine()) /*read and parse data*/
 			{
 					String line = sc.nextLine();
 					String[] vals = COMMA_PATTERN.split(line);
-					String[] date = vals[1].split("/");
 					
+					//Create criminal case node
 					Label case_label =  DynamicLabel.label("CASE");
 	    			Node CASE = DB.createNode(case_label);
-	    			CASE.setProperty("Case_id", vals[0]);//set case id 
+	    			CASE.setProperty("CASE_ID", vals[0]);//set case id 
 					
-					//Create or Grab Month Node of current case
-					Node month = months.getOrCreate("Month", date[0]);
-					CASE.createRelationshipTo(month, RelTypes.HAPPENED_IN);
-					
-					//Create or Grab Year Node of current case
-					Node year = years.getOrCreate("Year", date[2]);
-					CASE.createRelationshipTo(year, RelTypes.HAPPENED_IN);
-					
-					Node crime = crimes.getOrCreate("Crime", vals[4]);
-					CASE.createRelationshipTo(crime, RelTypes.COMMITTED);
-					
-					Node district = districts.getOrCreate("District", vals[10]);
-					CASE.createRelationshipTo(district, RelTypes.WHERE);
-					
-					Node beat = beats.getOrCreate("Beat", vals[9]);
-					CASE.createRelationshipTo(beat, RelTypes.WHERE);
-					
-					Node community = communities.getOrCreate("Community", vals[9]);
-					CASE.createRelationshipTo(community, RelTypes.WHERE);
-					
-					Node ld = loc_descriptions.getOrCreate("LocationDescription", vals[6]);
-					CASE.createRelationshipTo(community, RelTypes.WHERE);
+	    			//Connect the Criminal CASE to all other nodes in graph
+	    			loadDomestic(DB, domestic, CASE, vals);
+	    			loadArrest(DB, arrest, CASE, vals);
+	    			loadAMPM(DB, ampm, CASE, vals);
+	    			loadMonth(DB, months, CASE, vals);
+	    			loadYear(DB, years, CASE, vals);
+					loadCrime(DB, crimes, CASE, vals);	
+					loadDistrict(DB,districts, CASE, vals);
+					loadBeat(DB, beats, CASE, vals);
+					loadCommunity(DB, communities, CASE, vals);
+					loadLocDescription(DB, loc_descriptions, CASE, vals);
 					
 					//Flush the heap of the transaction and start a new transaction
 					if(++heapCount % batchSize == 0)
@@ -113,7 +103,7 @@ public class NeoParser
 						tx.success();
 						tx.close();
 						tx = DB.beginTx();
-						System.out.println(heapCount);
+						System.out.println(heapCount + " flushed");
 					}
 				
 			}//end while
@@ -123,7 +113,7 @@ public class NeoParser
 		catch(Exception e){e.printStackTrace();}
 	}//end buildDB
 	
-	//Create an index for a group of nodes
+	//Create an index for a group of nodes, with the same name for label and internal property
 	private UniqueFactory.UniqueNodeFactory getIndex(GraphDatabaseService DB, final String index)
 	{
 		try (Transaction tx = DB.beginTx())
@@ -142,5 +132,130 @@ public class NeoParser
 	    }
 	}
 	
+	//All methods below either create or get an indexed node and connect it to the CASE node for a crime
+	//All relationships are double-edged for versatile query paths
+	//There are differences in properties on the edges and internally on some indexes
 	
+	private void loadArrest(GraphDatabaseService DB, UniqueFactory.UniqueNodeFactory arrest, Node CASE, String [] vals)
+	{
+		Node arrested;
+		Relationship rel1, rel2;
+		
+		if(Boolean.parseBoolean(vals[7]))
+			arrested = arrest.getOrCreate("ARREST", true);
+		else
+			arrested = arrest.getOrCreate("ARREST", false);
+		
+		rel1 = CASE.createRelationshipTo(arrested, RelTypes.ARRESTED);
+		rel2 = arrested.createRelationshipTo(CASE, RelTypes.ARRESTED);
+	}
+	
+	
+	private void loadDomestic(GraphDatabaseService DB, UniqueFactory.UniqueNodeFactory domestic, Node CASE, String [] vals)
+	{
+		Node d;
+		Relationship rel1, rel2;
+		
+		if(Boolean.parseBoolean(vals[8]))
+			d = domestic.getOrCreate("DOMESTIC", true);
+		else
+			d = domestic.getOrCreate("DOMESTIC", false);
+		
+		rel1 = CASE.createRelationshipTo(d, RelTypes.HOME_WHERE);
+		rel2 = d.createRelationshipTo(CASE, RelTypes.HOME_WHERE);
+		
+	}
+	//Two nodes (AM or PM) in entire graph are indexed as AM/PM, their relationships store the HOUR and MINUTE of incident
+	private void loadAMPM(GraphDatabaseService DB, UniqueFactory.UniqueNodeFactory ampm, Node CASE, String [] vals)
+	{
+		Relationship rel1, rel2;
+		int hour = Integer.parseInt(vals[2].split(":")[0]);
+		int minute = Integer.parseInt(vals[2].split(":")[1]);
+		Node time = ampm.getOrCreate("AM/PM", vals[3]);
+		rel1 = CASE.createRelationshipTo(time, RelTypes.TIME_WHEN);
+		rel2 = time.createRelationshipTo(CASE, RelTypes.TIME_WHEN);
+		rel1.setProperty("HOUR", hour);
+		rel2.setProperty("HOUR", hour);
+		rel1.setProperty("MINUTE", minute);
+		rel2.setProperty("MINUTE", minute);
+	}
+	
+	private void loadMonth(GraphDatabaseService DB, UniqueFactory.UniqueNodeFactory months, Node CASE, String [] vals)
+	{
+		Relationship rel1, rel2;
+		//Create or Grab Month Node of current case
+		Node month = months.getOrCreate("MONTH", Integer.parseInt(vals[1].split("/")[0]));
+		
+		rel1 = CASE.createRelationshipTo(month, RelTypes.MONTH_WHEN);
+		rel2 = month.createRelationshipTo(CASE, RelTypes.MONTH_WHEN);
+		
+		rel1.setProperty("DAY", Integer.parseInt(vals[1].split("/")[1]));
+		rel2.setProperty("DAY", Integer.parseInt(vals[1].split("/")[1]));
+	}
+	private void loadYear(GraphDatabaseService DB, UniqueFactory.UniqueNodeFactory years, Node CASE, String [] vals)
+	{	
+		Relationship rel1, rel2;
+		//Create or Grab Year Node of current case
+		Node year = years.getOrCreate("YEAR", Integer.parseInt(vals[1].split("/")[2]));
+		
+		rel1 = CASE.createRelationshipTo(year, RelTypes.YEAR_WHEN);
+		rel2 = year.createRelationshipTo(CASE, RelTypes.YEAR_WHEN);
+		
+		rel1.setProperty("MONTH", Integer.parseInt(vals[1].split("/")[0]));
+		rel2.setProperty("MONTH", Integer.parseInt(vals[1].split("/")[0]));
+	}
+	private void loadCrime(GraphDatabaseService DB, UniqueFactory.UniqueNodeFactory crimes, Node CASE, String [] vals)
+	{
+		Relationship rel1, rel2;
+		Node crime = crimes.getOrCreate("CRIME", vals[4]);
+		
+		rel1 = CASE.createRelationshipTo(crime, RelTypes.COMMITTED);
+		rel2 = crime.createRelationshipTo(CASE, RelTypes.COMMITTED);
+		
+		if(Boolean.parseBoolean(vals[7]))
+		{
+			rel1.setProperty("ARRESTED", true);
+			rel2.setProperty("ARRESTED", true);
+		}
+		else
+		{
+			rel1.setProperty("ARRESTED", false);
+			rel2.setProperty("ARRESTED", false);
+		}
+		
+	}
+	private void loadDistrict(GraphDatabaseService DB, UniqueFactory.UniqueNodeFactory districts, Node CASE, String [] vals)
+	{
+		Relationship rel1, rel2;
+		Node district = districts.getOrCreate("DISTRICT", vals[10]);
+		rel1= CASE.createRelationshipTo(district, RelTypes.DISTRICT_WHERE);
+		rel1.setProperty("LATITUDE", vals[13]);
+		rel1.setProperty("LONGITUDE", vals[14]);
+		rel2 = district.createRelationshipTo(CASE, RelTypes.DISTRICT_WHERE);
+		rel2.setProperty("LATITUDE", vals[13]);
+		rel2.setProperty("LONGITUDE", vals[14]);
+	}
+	private void loadBeat(GraphDatabaseService DB, UniqueFactory.UniqueNodeFactory beats, Node CASE, String [] vals)
+	{
+		Node beat = beats.getOrCreate("BEAT", vals[9]);
+		CASE.createRelationshipTo(beat, RelTypes.BEAT_WHERE);
+		beat.createRelationshipTo(CASE, RelTypes.BEAT_WHERE);
+	}
+	private void loadCommunity(GraphDatabaseService DB, UniqueFactory.UniqueNodeFactory communities, Node CASE, String [] vals)
+	{
+		Relationship rel1, rel2;
+		Node community = communities.getOrCreate("COMMUNITY", vals[12]);
+		rel1 = CASE.createRelationshipTo(community, RelTypes.COMMUNITY_WHERE);
+		rel2 = community.createRelationshipTo(CASE, RelTypes.COMMUNITY_WHERE);
+		rel1.setProperty("LATITUDE", vals[13]);
+		rel1.setProperty("LONGITUDE", vals[14]);
+		rel2.setProperty("LATITUDE", vals[13]);
+		rel2.setProperty("LONGITUDE", vals[14]);
+	}
+	private void loadLocDescription(GraphDatabaseService DB, UniqueFactory.UniqueNodeFactory loc_descriptions, Node CASE, String [] vals)
+	{
+		Node ld = loc_descriptions.getOrCreate("LOCATION_DESCRIPTION", vals[6]);
+		CASE.createRelationshipTo(ld, RelTypes.LOCATION_WHERE);
+		ld.createRelationshipTo(CASE, RelTypes.LOCATION_WHERE);
+	}
 }
--- Neo4J/Tester.java
+++ Neo4J/Tester.java
@@ -1,5 +1,9 @@
 package org.neo4j.neo;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
@@ -8,20 +12,32 @@ public class Tester
 
 	public static void main(String[] args) 
 	{
-		String path = "C:\\Users\\Matthew\\Desktop\\MyNeoDB";
-		//NeoDB db = new NeoDB();
-		//buildDB(db); 
-		//doQueries(db);
-		//db.shutdown();
+		 try {
+			String current = new java.io.File( "." ).getCanonicalPath();
+			System.out.println(current);
+			
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		String path = "C:\\Users\\Matthew\\Desktop\\MyNeoDB_fullData";
 		GraphDatabaseService DB = new GraphDatabaseFactory().newEmbeddedDatabase(path);
 		System.out.println("Data Base created at: " + path);
 		buildDB(DB);
 		System.out.println("Done");
-	
+		DB.shutdown();
 	}
 	public static void buildDB(GraphDatabaseService DB)
 	{
-		NeoParser parser = new NeoParser("crimes.csv", DB); //smallData.txt
+		String filename;
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Enter the file name to parse:");
+		filename = kb.nextLine();
+		NeoParser parser = new NeoParser(filename, DB);
 	}
 	public static void doQueries(GraphDatabaseService DB)
 	{
